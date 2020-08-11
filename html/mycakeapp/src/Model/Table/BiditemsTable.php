960384ff8ce47c953add412ff9d791f7474f82b0
MUnehiroMachida <Life1231@outlook.jp>
2020-08-11 10:59:58 +0900

    画像拡張子チェック機能

--- html/mycakeapp/src/Model/Table/BiditemsTable.php
+++ html/mycakeapp/src/Model/Table/BiditemsTable.php
@@ -83,6 +83,10 @@ class BiditemsTable extends Table
             ->notEmptyString('information');
 
         $validator
+            ->scalar('image_path')
+            ->notEmptyString('image_path');
+
+        $validator
             ->dateTime('endtime')
             ->requirePresence('endtime', 'create')
             ->notEmptyDateTime('endtime');
